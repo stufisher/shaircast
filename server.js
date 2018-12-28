0fc3ff606f5d03374b91943aa63f2b65d5766cc5
stufisher <stu@q3f.org>
2018-12-28 15:23:59 +0100

    allow config to define port, add volume scaling, synchronise metadata

--- server.js
+++ server.js
@@ -11,15 +11,24 @@ var endpoints = config.endpoints;
 var devices;
 var currentStream;
 
+
 server.on("clientConnected", function(stream) {
 	console.log("clientConnected");
 	devices = [];
 	endpoints.forEach(function(host) {
-		devices.push(airtunes.add(host));
+		var dev = airtunes.add(host.host, { port: host.port, volscale: host.volscale });		
+		dev.on('status', function(status) {
+			console.log('Status', dev.key, status)
+		})
+		dev.on('error', function(err) {
+			console.log('Error', dev.key, error)
+		})
+		devices.push(dev)
 	});
 	currentStream = stream;
 	stream.pipe(airtunes);
 });
+
 
 server.on("clientDisconnected", function() {
 	console.log("clientDisconnected");
@@ -33,8 +42,30 @@ server.on("error", function(err) {
 	console.log("Error: " + err.message );
 });
 
+
+server.on("progressChange", function(progress) {
+	// console.log('progressChange', progress)
+	devices.forEach(function(device) {
+		device.setProgress(progress)
+	})
+})
+
+server.on("artworkChange", function(artwork) {
+	// console.log('artworkChange', artwork)
+	devices.forEach(function(device) {
+		device.setArtwork(artwork, 'image/jpeg')
+	})
+})
+
 server.on("metadataChange", function(metadata) {
-	console.log("Now playing \"" + metadata.minm + "\" by " + metadata.asar + " from \"" + metadata.asal + "\"");
+	// console.log(metadata)
+	console.log("Now playing \"" + metadata.minm + "\" by " + metadata.asar + " from \"" + metadata.asal + "\" len \"" + metadata.astm + "\"");
+	devices.forEach(function(device) {
+		device.setTrackInfo(metadata.minm, metadata.asar ? metadata.asar : '', metadata.asal ? metadata.asal : '')
+		if (metadata.astm) {
+			device.setTrackLength(metadata.astm)
+		}
+	})
 });
 
 server.on("volumeChange", function(volume) {
